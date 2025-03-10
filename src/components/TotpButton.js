a774b738121ce72b82aa29e4fd483a615da91ece
Andrew Ry <RyAndrew@users.noreply.github.com>
2025-03-09 19:14:37 -0500

    add copy totp button

--- src/components/TotpButton.js
+++ src/components/TotpButton.js
@@ -11,27 +11,47 @@ const TotpButton = ({totpSeed}) => {
     }
     setTotpResult(result)
   }
+
+  const copyToClipboard = () => {
+    if (totpResult) {
+      navigator.clipboard.writeText(totpResult);
+    }
+  };
   
   return (
     <div className="totp-button container mt-3">
-      <div className="row">
-        <div className="col-12">
-          <div className="input-group">
-            <button 
-              className="btn btn-retro btn-retro-primary totp-button" 
-              type="button" 
-              onClick={clickMfa}
-            >
-              Generate TOTP
-            </button>
-            <input 
-              type="text" 
-              className="form-control mfa-totp-textfield" 
-              placeholder="TOTP code will appear here" 
-              value={totpResult} 
-              readOnly 
-              aria-label="TOTP result"
-            />
+      <div className="row g-2 align-items-center">
+        <div className="col-12 col-sm-auto mb-2 mb-sm-0">
+          <button 
+            className="btn btn-retro btn-retro-primary totp-button w-100" 
+            type="button" 
+            onClick={clickMfa}
+          >
+            Generate TOTP
+          </button>
+        </div>
+        <div className="col-12 col-sm-auto">
+          <div className="row g-0">
+            <div className="col-8">
+              <input 
+                type="text" 
+                className="form-control mfa-totp-textfield border-0 rounded-0" 
+                value={totpResult} 
+                readOnly 
+                aria-label="TOTP result"
+              />
+            </div>
+            <div className="col-4">
+              <button 
+                className="btn btn-retro btn-retro-secondary w-100 rounded-0" 
+                type="button" 
+                onClick={copyToClipboard}
+                disabled={!totpResult}
+                title="Copy TOTP code"
+              >
+                <i className="bi bi-clipboard"></i>
+              </button>
+            </div>
           </div>
         </div>
       </div>
