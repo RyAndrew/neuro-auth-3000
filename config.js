4d1f36aff1ec84ff4c1d7f1f97b6febfcb2648ba
Andrew Ry <RyAndrew@users.noreply.github.com>
2025-03-17 23:13:26 -0500

    change global var to global function

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
-const oktaConfig = {
-  clientId: '0oapkxrbm34SQC9NT697',  
-  issuer: 'https://dev-5069239.okta.com',
-  redirectUri: window.location.origin + '/login',
-  scopes: ['openid', 'profile', 'email','phone'],
+function getOktaConfig(){
+  return {
+    clientId: '0oapkxrbm34SQC9NT697',  
+    issuer: 'https://dev-5069239.okta.com',
+    redirectUri: window.location.origin + '/login',
+    scopes: ['openid', 'profile', 'email','phone','refresh_token'],
+  }
 }
 
-const totpSeed = 'PG7YVBG4AYFSDEL3'
+function getTotpSeed(){
+  return 'PG7YVBG4AYFSDEL3'
+}
